e7dbb029b90d8975bf943fb1eeef3f2a64229107
odhiahmad <odhiahmad15@gmail.com>
2023-01-19 06:30:33 +0700

    :wrench: change to using without devtools saga

--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,14 @@ import { createWrapper } from "next-redux-wrapper";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
 import rootSaga from "./sagas";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 let store;
 const sagaMiddleware = createSagaMiddleware();
 store = createStore(
   rootReducer,
-  // applyMiddleware(sagaMiddleware),
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
+  applyMiddleware(sagaMiddleware)
+  // composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
